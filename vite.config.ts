c9ce50d7d621f708dcfb796744dbcf3fcb7bf70c
Job Celaya <job.celaya@dispatchhealth.com>
2024-03-14 20:04:48 -0600

    feat: update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   //path used for github pages https://jobceg.github.io/autocomplete/
-  base: 'autocomplete',
+  base: '/autocomplete',
   plugins: [react()],
   test: {
     coverage: {
