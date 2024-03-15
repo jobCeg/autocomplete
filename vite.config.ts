64b7011f087c294bf344f0649fbb64f1a0b71012
Job Celaya <job.celaya@dispatchhealth.com>
2024-03-14 18:33:27 -0600

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    coverage: {
+      reporter: ['html', 'text', 'json-summary', 'json'],
+      thresholds: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./setupTest.ts'],
+  },
+});
