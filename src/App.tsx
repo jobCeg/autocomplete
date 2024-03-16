de14c6662a3e409640b773dcb6d17466b35eac5c
Job Celaya <job.celaya@dispatchhealth.com>
2024-03-15 22:15:38 -0600

    feat: solve types coverage issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import './App.css';
 import { Autocomplete } from './components/Autocomplete/Autocomplete';
-import { useLazyQuery } from './hooks/useLazyQuery';
-import { getUsers } from './features/users/api/getUsers';
-import type { QueryFnResponseType, QueryFnType } from './features/users/api/getUsers';
+// import { useLazyQuery } from './hooks/useLazyQuery';
+// import { getUsers } from './features/users/api/getUsers';
+// import type { QueryFnResponseType, QueryFnType } from './features/users/api/getUsers';
 
 function App() {
-  const { abort, data, loading, fetch } = useLazyQuery<QueryFnType, QueryFnResponseType>({
-    queryFn: getUsers,
-  });
+  // const { abort, data, loading, fetch } = useLazyQuery<QueryFnType, QueryFnResponseType>({
+  //   queryFn: getUsers,
+  // });
 
   return (
     <div data-testid="app">
       <h2>Autocomplete</h2>
-      <h3>{loading && <p>Loading...</p>}</h3>
+      {/* <h3>{loading && <p>Loading...</p>}</h3> */}
       <Autocomplete />
-      <ul>{!!data && data.slice(0, 5).map((user) => <li key={user.id}>{user.name}</li>)}</ul>
+      {/* <ul>{!!data && data.slice(0, 5).map((user) => <li key={user.id}>{user.name}</li>)}</ul> */}
 
-      <button onClick={() => fetch({ body: '' })}>fetch</button>
-      <button onClick={() => abort()}>cancel</button>
+      {/* <button onClick={() => fetch({ body: '' })}>fetch</button>
+      <button onClick={() => abort()}>cancel</button> */}
     </div>
   );
 }
